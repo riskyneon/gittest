1f9581867a653a037f3be2f48296747f514adf48
bharatpattani <aksharbharat9@gmail.com>
2014-05-29 13:26:49 +0530

    initial commit

---
+++ test.php
@@ -0,0 +1,29 @@
+<?php
+//Getting the org_ids from tbl_org
+$db_hostname="localhost";
+$db_username="root";
+$db_password="";
+$db_databasename="test";
+
+
+$con=mysql_connect($db_hostname,$db_username,$db_password);
+if(!$con)
+{
+	die("Database connection failed miserably: " . mysql_error());
+}
+$db=mysql_select_db($db_databasename,$con);
+if(!$db)
+{
+	die("Database selection also failed miserably: " . mysql_error());
+}
+$result = mysql_query("INSERT INTO test (id) values (1)", $con);
+if(!$result)
+{
+
+	die("mysql query failed miserably: " . mysql_error());
+}
+
+
+
+mysql_close();
+?>
---
+++ test_exec.php
@@ -0,0 +1,8 @@
+<?php    
+echo (date('M'));
+echo date("M", strtotime("-1 months"));
+echo date("Y");
+echo date("d");
+echo dirname(__FILE__);
+
+ ?>
